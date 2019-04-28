51d45be0f96e0aa772f7dddf83c2367e29319751
phuocantd <phuocantd@gmail.com>
2019-04-28 19:14:39 +0700

    init file

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBepw3vbdeURnN6psKEc49DNV-kYtT-G_I",
+    authDomain: "login-firebase-80b75.firebaseapp.com",
+    databaseURL: "https://login-firebase-80b75.firebaseio.com",
+    projectId: "login-firebase-80b75",
+    storageBucket: "login-firebase-80b75.appspot.com",
+    messagingSenderId: "211923537382"
+};
+firebase.initializeApp(config);
